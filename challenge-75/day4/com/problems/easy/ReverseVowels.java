cea89892f08b5e0d6e9ae40b13244ee0b38f0cc0
Venkat Nandanavanam <vnandana@akamai.com>
2023-12-08 23:31:16 +0530

    leetcode75 day 4 checkin

--- challenge-75/day4/com/problems/easy/ReverseVowels.java
+++ challenge-75/day4/com/problems/easy/ReverseVowels.java
@@ -3,6 +3,32 @@ package com.problems.easy;
 import java.util.ArrayList;
 import java.util.List;
 
+/*
+   Given a string s, reverse only all the vowels in the string and return it.
+
+    The vowels are 'a', 'e', 'i', 'o', and 'u', and they can appear in both lower and upper cases, more than once.
+
+
+
+    Example 1:
+
+    Input: s = "hello"
+    Output: "holle"
+
+    Example 2:
+
+    Input: s = "leetcode"
+    Output: "leotcede"
+
+
+
+    Constraints:
+
+        1 <= s.length <= 3 * 105
+        s consist of printable ASCII characters.
+
+
+ */
 public class ReverseVowels {
     char[] vowels = new char[]{'a','e','i','o','u', 'A','E','I','O','U'};
     public String reverseVowels(String s) {

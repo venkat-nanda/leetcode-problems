9fd42cfa9e598eba728e3945549f15eba822c37c
Venkat Nandanavanam <vnandana@akamai.com>
2023-12-24 01:57:27 +0530

    leetcode75 sliding window and prefix sum problem solutions

---
+++ challenge-75/com/problems/easy/MaxAvg.java
@@ -0,0 +1,50 @@
+package com.problems.easy;
+
+/*
+     You are given an integer array nums consisting of n elements, and an integer k.
+
+    Find a contiguous subarray whose length is equal to k that has the maximum average value and return this value. Any answer with a calculation error less than 10-5 will be accepted.
+
+
+
+    Example 1:
+
+    Input: nums = [1,12,-5,-6,50,3], k = 4
+    Output: 12.75000
+    Explanation: Maximum average is (12 - 5 - 6 + 50) / 4 = 51 / 4 = 12.75
+
+    Example 2:
+
+    Input: nums = [5], k = 1
+    Output: 5.00000
+
+
+
+    Constraints:
+
+        n == nums.length
+        1 <= k <= n <= 105
+        -104 <= nums[i] <= 104
+
+
+ */
+public class MaxAvg {
+
+    public double findMaxAverage(int[] nums, int k) {
+
+        int max_sum = 0;
+
+        for(int i=0; i < k ; i++){
+            max_sum += nums[i];
+        }
+
+        int window_sum = max_sum;
+        for(int i=k ; i < nums.length ; i++){
+            window_sum = window_sum - nums[ i- k] + nums[i];
+            max_sum = Math.max(window_sum, max_sum);
+        }
+
+        return (double) max_sum / k ;
+    }
+
+}
